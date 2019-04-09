743367b4d0c231b952c7657ca01f48969087146b
Sanal Dhamanse <sanal.dhamanse2131926@gmail.com>
2019-04-09 23:59:08 +0530

    rech page 57

--- OOP/basic-app/src/com/teclabs/chapter/Chapter1.java
+++ OOP/basic-app/src/com/teclabs/chapter/Chapter1.java
@@ -6,6 +6,20 @@ public class Chapter1 {
 		System.out.println("I sanal");
 		System.out.println("The world");
 		int x = 3;
+		String []wordListOne={"24/7","multi-Taier","30,200 foot","B-to-B","win-win","front-end"};
+		String[] wordListtwo={"emposed","sticky","value-add","oriented","centric","distributed","clustered","braanded"};
+		String []wordLoistThree={"process", "tippingpoint", "solution", "architecture", "core competency","strategy", "mindshare", "portal","space", "vision","paradigm", "mission"};
+		
+		int oneLength = wordListOne.length;
+		 int twoLength = wordListtwo.length;
+		 int threeLength = wordLoistThree.length;
+		 
+		 int rand1 = (int) (Math.random() * oneLength);
+		 int rand2 = (int) (Math.random() * twoLength);
+		 int rand3 = (int) (Math.random() * threeLength);
+		 String phrase = wordListOne[rand1] + " " +		 wordListtwo[rand2] + " "+ wordLoistThree[rand3];
+		 
+		 System.out.println("What we need is a " + phrase);
 		String name = "Dirk";
 		x = x * 17;
 		System.out.println("X is " + x);
